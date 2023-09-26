f7d0e000a48dc981baf0b2fdbc34e096f374b832
Prince Mahmud <princey01767@gmail.com>
2023-09-26 23:08:50 +0600

    initial release

---
+++ lib/flutter_paypal_checkout.dart
@@ -0,0 +1,305 @@
+library flutter_paypal_checkout;
+
+import 'dart:core';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_paypal_checkout/repositories/PaypalServices.dart';
+import 'package:flutter_paypal_checkout/repositories/network_error.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+// Import for Android features.
+import 'package:webview_flutter_android/webview_flutter_android.dart';
+// Import for iOS features.
+import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
+
+
+class UsePaypal extends StatefulWidget {
+  final Function onSuccess, onCancel, onError;
+  final String returnURL, cancelURL, note, clientId, secretKey;
+  final List transactions;
+  final bool sandboxMode;
+  const UsePaypal({
+    Key? key,
+    required this.onSuccess,
+    required this.onError,
+    required this.onCancel,
+    required this.returnURL,
+    required this.cancelURL,
+    required this.transactions,
+    required this.clientId,
+    required this.secretKey,
+    this.sandboxMode = false,
+    this.note = '',
+  }) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return UsePaypalState();
+  }
+}
+
+class UsePaypalState extends State<UsePaypal> {
+  String checkoutUrl = '';
+  String navUrl = '';
+  String executeUrl = '';
+  String accessToken = '';
+  bool loading = true;
+  bool pageloading = true;
+  bool loadingError = false;
+  late PaypalServices services;
+  int pressed = 0;
+
+  Map getOrderParams() {
+    Map<String, dynamic> temp = {
+      "intent": "sale",
+      "payer": {"payment_method": "paypal"},
+      "transactions": widget.transactions,
+      "note_to_payer": widget.note,
+      "redirect_urls": {
+        "return_url": widget.returnURL,
+        "cancel_url": widget.cancelURL
+      }
+    };
+    return temp;
+  }
+
+  loadPayment() async {
+    setState(() {
+      loading = true;
+    });
+    try {
+      Map getToken = await services.getAccessToken();
+      if (getToken['token'] != null) {
+        accessToken = getToken['token'];
+        final transactions = getOrderParams();
+        final res =
+        await services.createPaypalPayment(transactions, accessToken);
+        if (res["approvalUrl"] != null) {
+          setState(() {
+            checkoutUrl = res["approvalUrl"].toString();
+            navUrl = res["approvalUrl"].toString();
+            executeUrl = res["executeUrl"].toString();
+            loading = false;
+            pageloading = false;
+            loadingError = false;
+          });
+        } else {
+          widget.onError(res);
+          setState(() {
+            loading = false;
+            pageloading = false;
+            loadingError = true;
+          });
+        }
+      } else {
+        widget.onError("${getToken['message']}");
+
+        setState(() {
+          loading = false;
+          pageloading = false;
+          loadingError = true;
+        });
+      }
+    } catch (e) {
+      widget.onError(e);
+      setState(() {
+        loading = false;
+        pageloading = false;
+        loadingError = true;
+      });
+    }
+  }
+  // Enable hybrid composition.
+  late final WebViewController _controller;
+
+
+  @override
+  void initState() {
+    super.initState();
+    services = PaypalServices(
+      sandboxMode: widget.sandboxMode,
+      clientId: widget.clientId,
+      secretKey: widget.secretKey,
+    );
+    setState(() {
+      navUrl = widget.sandboxMode
+          ? 'https://api.sandbox.paypal.com'
+          : 'https://www.api.paypal.com';
+    });
+
+      // #docregion platform_features
+      late final PlatformWebViewControllerCreationParams params;
+      if (WebViewPlatform.instance is WebKitWebViewPlatform) {
+        params = WebKitWebViewControllerCreationParams(
+          allowsInlineMediaPlayback: true,
+          mediaTypesRequiringUserAction: const <PlaybackMediaTypes>{},
+        );
+      } else {
+        params = const PlatformWebViewControllerCreationParams();
+      }
+
+      final WebViewController controller =
+      WebViewController.fromPlatformCreationParams(params);
+      // #enddocregion platform_features
+
+      controller
+        ..setJavaScriptMode(JavaScriptMode.unrestricted)
+        ..setBackgroundColor(const Color(0x00000000))
+        ..setNavigationDelegate(
+          NavigationDelegate(
+            onProgress: (int progress) {
+              debugPrint('WebView is loading (progress : $progress%)');
+            },
+            onPageStarted: (String url) {
+              debugPrint('Page started loading: $url');
+            },
+            onPageFinished: (String url) {
+              debugPrint('Page finished loading: $url');
+            },
+            onWebResourceError: (WebResourceError error) {
+              debugPrint('''
+Page resource error:
+  code: ${error.errorCode}
+  description: ${error.description}
+  errorType: ${error.errorType}
+  isForMainFrame: ${error.isForMainFrame}
+          ''');
+            },
+            onNavigationRequest: (NavigationRequest request) {
+              if (request.url.startsWith('https://www.youtube.com/')) {
+                debugPrint('blocking navigation to ${request.url}');
+                return NavigationDecision.prevent;
+              }
+              debugPrint('allowing navigation to ${request.url}');
+              return NavigationDecision.navigate;
+            },
+            onUrlChange: (UrlChange change) {
+              debugPrint('url change to ${change.url}');
+            },
+          ),
+        )
+        ..addJavaScriptChannel(
+          'Toaster',
+          onMessageReceived: (JavaScriptMessage message) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              widget.onError(message),
+            );
+          },
+        )
+        ..loadRequest(Uri.parse(checkoutUrl));
+
+      // #docregion platform_features
+      if (controller.platform is AndroidWebViewController) {
+        AndroidWebViewController.enableDebugging(true);
+        (controller.platform as AndroidWebViewController)
+            .setMediaPlaybackRequiresUserGesture(false);
+      }
+      // #enddocregion platform_features
+
+      _controller = controller;
+    loadPayment();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async {
+        if (pressed < 2) {
+          setState(() {
+            pressed++;
+          });
+          final snackBar = SnackBar(
+              content: Text(
+                  'Press back ${3 - pressed} more times to cancel transaction'));
+          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+          return false;
+        } else {
+          return true;
+        }
+      },
+      child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: const Color(0xFF272727),
+            leading: GestureDetector(
+              child: const Icon(Icons.arrow_back_ios),
+              onTap: () => Navigator.pop(context),
+            ),
+            title: Row(
+              children: [
+                Expanded(
+                    child: Container(
+                      padding:
+                      const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                      decoration: BoxDecoration(
+                          color: Colors.black.withOpacity(0.3),
+                          borderRadius: BorderRadius.circular(20)),
+                      child: Row(
+                        children: [
+                          Icon(
+                            Icons.lock_outline,
+                            color: Uri.parse(navUrl).hasScheme
+                                ? Colors.green
+                                : Colors.blue,
+                            size: 18,
+                          ),
+                          const SizedBox(width: 5),
+                          Expanded(
+                            child: Text(
+                              navUrl,
+                              overflow: TextOverflow.ellipsis,
+                              style: const TextStyle(fontSize: 14),
+                            ),
+                          ),
+                          SizedBox(width: pageloading ? 5 : 0),
+                          pageloading
+                              ? const SpinKitFadingCube(
+                            color: Color(0xFFEB920D),
+                            size: 10.0,
+                          )
+                              : const SizedBox()
+                        ],
+                      ),
+                    ))
+              ],
+            ),
+            elevation: 0,
+          ),
+          body: SizedBox(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            child: loading
+                ? Column(
+              children: const [
+                Expanded(
+                  child: Center(
+                    child: SpinKitFadingCube(
+                      color: Color(0xFFEB920D),
+                      size: 30.0,
+                    ),
+                  ),
+                ),
+              ],
+            )
+                : loadingError
+                ? Column(
+              children: [
+                Expanded(
+                  child: Center(
+                    child: NetworkError(
+                        loadData: loadPayment,
+                        message: "Something went wrong,"),
+                  ),
+                ),
+              ],
+            )
+                : Column(
+              children: [
+                Expanded(
+                  child: WebViewWidget(controller: _controller),
+                ),
+              ],
+            ),
+          )),
+    );
+  }
+}
